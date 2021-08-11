4da4ab768125f31a19dedec467c68aa7edc10e6b
Joaohbs18 <88114417+Joaohbs18@users.noreply.github.com>
2021-08-11 20:05:24 -0300

    fim da aula

--- classes.py
+++ classes.py
@@ -1,2 +1,26 @@
-class Point():
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers= []
+        
+    def add_pasenger(self,name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+    
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+
+Flight - Flight(3)
+
+people = ["Harry", "Ron", "Hermione", "Ginny"]
+for person in people:
+    sucess: Flight.add_passanger(person)
+    if sucess:
+        print(f"Added {person} to flight sucessfully.")
+    else:
+        print(f"No avaliable seats for {person}. ")
+        
     
